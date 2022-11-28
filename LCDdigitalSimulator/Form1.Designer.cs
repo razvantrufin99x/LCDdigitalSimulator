88a542857a3aac32b42f2e3ed18d5316da28ad07
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-11-28 03:18:56 +0200

    Add project files.

---
+++ LCDdigitalSimulator/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace LCDdigitalSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLCD = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "WRITE TEXT:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLCD
+            // 
+            this.txtLCD.Location = new System.Drawing.Point(158, 13);
+            this.txtLCD.Name = "txtLCD";
+            this.txtLCD.Size = new System.Drawing.Size(276, 20);
+            this.txtLCD.TabIndex = 1;
+            this.txtLCD.Text = "abcdefghijklmnopqrstuvxzwy0123456789";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 495);
+            this.Controls.Add(this.txtLCD);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "LCD DIGITAL SIMULATOR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLCD;
+    }
+}
+
---
+++ LCDdigitalSimulator/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace LCDdigitalSimulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Graphics g;
+        public Pen p = new Pen(Color.Black);
+        public Brush b = new SolidBrush(Color.Black);
+        public int len = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            len = txtLCD.Text.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                g.DrawString(txtLCD.Text.Substring(i, 1),this.Font,b,10+(i*7),50);
+                Thread.Sleep(20);
+
+            }
+        }
+    }
+}
